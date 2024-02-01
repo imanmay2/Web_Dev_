6c511a2498d1caff78bae7c28c820d12b8580e7b
manmay2 <imanmay2@gmail.com>
2024-02-01 18:54:15 +0530

    Q2 JS done( #mission JS)

--- PROGRAMS/1st.js
+++ PROGRAMS/1st.js
@@ -22,3 +22,20 @@ console.log(product); // Changed everything.......
 
 
 
+
+//2. ....Create a const variable where it wil store a person's insta id information.
+//   and do some operations on it.
+
+const insta={
+    id_name: "imanmay2",
+    followers: 47,
+    following: 51,
+    descrip:"i am learning Javascript"
+};
+
+console.log(insta);  // User entry is shown here.
+insta.id_name="manmaychakraborty";
+insta["followers"]=10;
+insta["following"]=20;
+insta.descrip="Ziel ist meine Deutschland , Deutschland ist meine Ziel";
+console.log(insta); // Info changed ......
