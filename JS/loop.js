dc091dd7713583169305088a8570414617bd6af6
manmay2 <imanmay2@gmail.com>
2024-02-03 12:37:20 +0530

    done with loops in js

--- JS/loop.js
+++ JS/loop.js
@@ -1,67 +1,66 @@
 
-// //Use of loop .
-// for(let i=1;i<=8;i++){
-//     console.log("WELCOME TO JAVASCRIPT");
-// }
+//Use of loop .
+for(let i=1;i<=8;i++){
+    console.log("WELCOME TO JAVASCRIPT");
+}
 
 
-// //USE OF WHILE LOOP.
-// let i=1;
-// while(i<10){
-//     console.log(i);
-//     i++;
-// }
-
-
-
-// // USE OF DO WHILE LOOP.   The loop will run for 1 time guaranteed.
-// let i=20;
-// do {
-//     console.log(i);
-//     i++;
-// } while(i<10);  // NOTE : here, at last semicolon is there.
+//USE OF WHILE LOOP.
+let i=1;
+while(i<10){
+    console.log(i);
+    i++;
+}
 
 
 
+// USE OF DO WHILE LOOP.   The loop will run for 1 time guaranteed.
+i=20;
+do {
+    console.log(i);
+    i++;
+} while(i<10);  // NOTE : here, at last semicolon is there.
 
 
-// // USE OF FOR-OF loop.
-// let str="Germany";
-// for(let i of str){
-//     console.log(i);
-// }
+
+
+
+// USE OF FOR-OF loop.
+let str="Germany";
+for(let i of str){
+    console.log(i);
+}
 
 
 
 
 //USE OF FOR-IN LOOP.
-// // used in OBJECTS.
-// let student = {
-//     name: "Manmay",
-//     age:20,
-//     cgpa:8.8,
-//     address: "Bangalore"
-// };
-
-// for(let key in student){
-//     console.log(key,":",student[key]);
-// }
-
+// used in OBJECTS.
+let student = {
+    name: "Manmay",
+    age:20,
+    cgpa:8.8,
+    address: "Bangalore"
+};
+for(let key in student){
+    console.log(key,":",student[key]);
+}
 
 
 
-                                        //PRACTISE Q.
+
+                                      //  PRACTISE Q.
 
 
 
-// // Q1. print all the Even and odd number from 1 to n.
-// let n=prompt("Enter the range : ");
-// console.log("Evens numbers : ");
-// for(let i=1;i<=n;i++){
-//     if(i%2==0){
-//         console.log(i);
-//     }
-// }
+// Q1. print all the Even and odd number from 1 to n.
+let n=prompt("Enter the range : ");
+console.log("Evens numbers : ");
+for(let i=1;i<=n;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
 
 
 
