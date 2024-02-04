fab6036650907d759e2a9091c399f4c99d51c3ef
manmay2 <imanmay2@gmail.com>
2024-02-04 18:07:39 +0530

    DONE WITH STRINGS IN JS

--- JS/string.js
+++ JS/string.js
@@ -1,40 +1,40 @@
 // Lets learn string in javasript....
 
 // // Q1. Count the letters present in the string.
-// let str="Germany";
-// console.log("Count of the string is  : ",str.length);
+let str="Germany";
+console.log("Count of the string is  : ",str.length);
 
 
 //Q2. Use the shorcut expression to print something from an object.
-// let obj= {
-//     object:"Macbook Air",
-//     price : "80000"
-// };
+let obj= {
+    object:"Macbook Air",
+    price : "80000"
+};
 
 
 
-// // use back text  for shortcut.
-// output=`I have purchased ${obj.object} and  \n it costed around ${obj.price} `;
-// console.log(output);
+// use back text  for shortcut.
+output=`I have purchased ${obj.object} and  \n it costed around ${obj.price} `;
+console.log(output);
 
 
 
-// // UPPERCASE, LOWERCASE AND TRIM
-// let str='   GERMANY     '; 
-// console.log(str.toUpperCase());
-// console.log(str.toLowerCase());
-// console.log(str.trim());
+// UPPERCASE, LOWERCASE AND TRIM
+let str='   GERMANY     '; 
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.trim());
 
 
 
 
-// // slicing , concatenation, index,replace
-// let str='germany';
-// console.log(str.slice(0,3));
-// console.log(str.concat(" Europe"));
-// console.log(str.charAt(4));
-// console.log(str.replace("g","G"));      // it will replace only the first matching character.
-// console.log(str.replaceAll("g","G"));   // it will replace all the matching character.
+// slicing , concatenation, index,replace
+let str='germany';
+console.log(str.slice(0,3));
+console.log(str.concat(" Europe"));
+console.log(str.charAt(4));
+console.log(str.replace("g","G"));      // it will replace only the first matching character.
+console.log(str.replaceAll("g","G"));   // it will replace all the matching character.
 
 
 
