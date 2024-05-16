2dd014650e14c897519624c21ff64f7c1ca8a7e9
manmay2 <imanmay2@gmail.com>
2024-05-16 09:33:32 +0530

    done with objects

---
+++ JS/classes_and_objects.js
@@ -0,0 +1,33 @@
+// Lets start with object....
+
+
+// const student={
+//     f_name:"Anwesha Chakraborty",
+//     marks:94.8,
+//     printMarks(){
+//         console.log(this.marks);
+//     }
+// }
+
+
+
+
+
+
+//creating own prototype
+// const Employee={
+//     Tax(){
+//         console.log("Tax is 10%");
+//     }
+// }
+
+// const manmay={
+//     salary:550000,
+//     Tax(){
+//         console.log("Tax is 20%");
+//     }
+// };
+// manmay.__proto__=Employee;
+
+
+
