6e3034e14a1ae764dcbc16367efd577dd154483b
manmay2 <imanmay2@gmail.com>
2024-10-12 00:26:32 +0530

    updated

---
+++ JS/app.js
@@ -0,0 +1,56 @@
+// Write a program that will  input the size of the popcorn and print the price.
+
+// let size=prompt("Enter the size of the popcorn : ");
+// if(size==='XL'){
+//     console.log("Price is Rs 250. ");
+// }
+// else if(size==="L"){
+//     console.log("Price is Rs 200");
+// }
+// else if(size==="M"){
+//     console.log("Price is Rs 100");
+// }
+// else if(size==="S"){
+//     console.log("Price is Rs 50");
+// }
+
+
+
+// // Write a program to check whether it is a good string or not.
+// str_=prompt("Enter the string : ");
+// if((str_[0]=='a' || str_[0]=='A') && str_.length>3){
+//     console.log(`${str_} is a good string. `);
+// }
+// else{
+//     console.log(`${str_} is not a good string. `);
+// }
+
+
+
+// Create a switch statement for printing the day in the week.
+let day = prompt("Enter the day - ")
+switch (day) {
+    case "1":
+        console.log("Sunday");
+        break;
+    case "2":
+        console.log("Sunday");
+        break;
+    case "3":
+        console.log("Sunday");
+        break;
+    case "4":
+        console.log("Sunday");
+        break;
+    case "5":
+        console.log("Sunday");
+        break;
+    case "6":
+        console.log("Sunday");
+        break;
+    case "7":
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Please enter between 1-7")
+}
