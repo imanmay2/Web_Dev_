afc9aa7369fef1dd098e1e6422c636ade9711123
manmay2 <imanmay2@gmail.com>
2024-10-12 16:37:12 +0530

    JS_part 3 done....

--- JS/JS_3.js
+++ JS/JS_3.js
@@ -4,7 +4,13 @@
 // let str2="College";
 // alert(`String after replacing ${str2.replace('l','t')}`);
 
-let month=["January","July","March","August"];
-month[0]="July";
-month[1]="June";
-console.log(month);
+// let month=["January","July","March","August"];
+// month[0]="July";
+// month[1]="June";
+// console.log(month);
+
+
+let arr_=['C','C++','HTML','Javascript','Python','Java','C#','SQL']
+arr_=arr_.reverse();
+console.log(arr_);
+console.log(`Index of Javascript is : ${arr_.indexOf('Javascript')}`);
