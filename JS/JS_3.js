cc9defc617eef79bf3584e6f75a9e9aedbcb96c9
manmay2 <imanmay2@gmail.com>
2024-10-12 10:48:50 +0530

    JS_part 3 , to be continued

---
+++ JS/JS_3.js
@@ -0,0 +1,10 @@
+// let str1="   msg!  ";
+// alert(`String after triming and uppercase is  :  ${str1.trim().toUpperCase()}`);
+
+// let str2="College";
+// alert(`String after replacing ${str2.replace('l','t')}`);
+
+let month=["January","July","March","August"];
+month[0]="July";
+month[1]="June";
+console.log(month);
