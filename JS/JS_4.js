7e1f896749fbee3e5919df8abdc22def298306b9
manmay2 <imanmay2@gmail.com>
2024-10-14 09:49:31 +0530

    Multiplication table added

--- JS/JS_4.js
+++ JS/JS_4.js
@@ -1,12 +1,19 @@
-// Write a Javascript for printing all the odd number and even number from 1 to 20.
+// // Write a Javascript for printing all the odd number and even number from 1 to 20.
 
-console.log("The odd numbers are : ");
-for(let i=1;i<=20;i=i+2){
-    console.log(i);
-}
-console.log();
-console.log();
-console.log("The even numbers are : ");
-for(let i=0;i<=20;i=i+2){
-    console.log(i);
+// console.log("The odd numbers are : ");
+// for(let i=1;i<=20;i=i+2){
+//     console.log(i);
+// }
+// console.log();
+// console.log();
+// console.log("The even numbers are : ");
+// for(let i=0;i<=20;i=i+2){
+//     console.log(i);
+// }
+
+//Write a Javascript code for printing a multiplication table of the inputted number.
+let n=prompt("Enter the number : ");
+console.log(`Multiplication Table of ${n} is : `);
+for(let i=1;i<=10;i++){
+    console.log(`${n} * ${i} = ${n*i}`);
 }
