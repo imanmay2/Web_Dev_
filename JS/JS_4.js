837ffea61462803ca1b01a1663df38a4c20d7a1f
manmay2 <imanmay2@gmail.com>
2024-10-14 09:36:11 +0530

    Javascript part 4 initiated

---
+++ JS/JS_4.js
@@ -0,0 +1,12 @@
+// Write a Javascript for printing all the odd number and even number from 1 to 20.
+
+console.log("The odd numbers are : ");
+for(let i=1;i<=20;i=i+2){
+    console.log(i);
+}
+console.log();
+console.log();
+console.log("The even numbers are : ");
+for(let i=0;i<=20;i=i+2){
+    console.log(i);
+}
