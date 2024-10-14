940b1eae7d14ba4807181834d541aa5cf52ccdc6
manmay2 <imanmay2@gmail.com>
2024-10-14 18:36:34 +0530

    JS_part 4 lecture done

--- JS/JS_4.js
+++ JS/JS_4.js
@@ -1,36 +1,38 @@
-// // Write a Javascript for printing all the odd number and even number from 1 to 20.
+// Write a Javascript for printing all the odd number and even number from 1 to 20.
 
-// console.log("The odd numbers are : ");
-// for(let i=1;i<=20;i=i+2){
-//     console.log(i);
-// }
-// console.log();
-// console.log();
-// console.log("The even numbers are : ");
-// for(let i=0;i<=20;i=i+2){
-//     console.log(i);
-// }
+console.log("The odd numbers are : ");
+for(let i=1;i<=20;i=i+2){
+    console.log(i);
+}
+console.log();
+console.log();
+console.log("The even numbers are : ");
+for(let i=0;i<=20;i=i+2){
+    console.log(i);
+}
 
-// //Write a Javascript code for printing a multiplication table of the inputted number.
-// let n=prompt("Enter the number : ");
-// console.log(`Multiplication Table of ${n} is : `);
-// for(let i=1;i<=10;i++){
-//     console.log(`${n} * ${i} = ${n*i}`);
-// }
+//Write a Javascript code for printing a multiplication table of the inputted number.
+let n=prompt("Enter the number : ");
+console.log(`Multiplication Table of ${n} is : `);
+for(let i=1;i<=10;i++){
+    console.log(`${n} * ${i} = ${n*i}`);
+}
 
 
 
-// // Guess my Favourite Movie.
-// let fav_movie="Tholiprema";
-// let f=0;
-// let curr_movie=prompt("Enter the movie : ");
-// while(fav_movie!=curr_movie){
-//     curr_movie=prompt("Wrong ! Enter the movie again : ");
-//     if(curr_movie=="quit"){
-//         f=1;
-//         break;
-//     }
-// }
-// if(f==0){
-//     alert("Correct Guess !");
-// }
+// Guess my Favourite Movie.
+let fav_movie="Tholiprema";
+let f=0;
+let curr_movie=prompt("Enter the movie : ");
+while(fav_movie!=curr_movie){
+    curr_movie=prompt("Wrong ! Enter the movie again : ");
+    if(curr_movie=="quit"){
+        f=1;
+        break;
+    }
+}
+if(f==0){
+    alert("Correct Guess !");
+}
+
+
