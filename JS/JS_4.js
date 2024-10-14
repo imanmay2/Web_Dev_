9ed52a7419eb701865b3428b1eadbe560b0a0382
manmay2 <imanmay2@gmail.com>
2024-10-14 10:02:49 +0530

    Favourite Movie guess done

--- JS/JS_4.js
+++ JS/JS_4.js
@@ -11,9 +11,26 @@
 //     console.log(i);
 // }
 
-//Write a Javascript code for printing a multiplication table of the inputted number.
-let n=prompt("Enter the number : ");
-console.log(`Multiplication Table of ${n} is : `);
-for(let i=1;i<=10;i++){
-    console.log(`${n} * ${i} = ${n*i}`);
-}
+// //Write a Javascript code for printing a multiplication table of the inputted number.
+// let n=prompt("Enter the number : ");
+// console.log(`Multiplication Table of ${n} is : `);
+// for(let i=1;i<=10;i++){
+//     console.log(`${n} * ${i} = ${n*i}`);
+// }
+
+
+
+// // Guess my Favourite Movie.
+// let fav_movie="Tholiprema";
+// let f=0;
+// let curr_movie=prompt("Enter the movie : ");
+// while(fav_movie!=curr_movie){
+//     curr_movie=prompt("Wrong ! Enter the movie again : ");
+//     if(curr_movie=="quit"){
+//         f=1;
+//         break;
+//     }
+// }
+// if(f==0){
+//     alert("Correct Guess !");
+// }
