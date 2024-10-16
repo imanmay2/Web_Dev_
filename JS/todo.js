79bcb161a95708a4cfa8cf170d2172ccffb68032
manmay2 <imanmay2@gmail.com>
2024-10-16 22:41:54 +0530

    TODO programmed

---
+++ JS/todo.js
@@ -0,0 +1,25 @@
+let task_arr=[];
+while(true){
+    let req=prompt("Enter the task : ").toLowerCase();
+    if(req=="list"){
+        for(let i=0;i<task_arr.length;i++){
+            console.log("--------------------");
+            console.log(`${i} ${task_arr[i]}`);
+            console.log("--------------------");
+        }
+    }
+    else if(req=="add"){
+        let task=prompt("Enter the task to be added : ");
+        task_arr.push(task);
+        console.log("Task Added.");
+    }
+    else if(req=="delete"){
+        let del=prompt("Enter the task index to be deleted : ");
+        task_arr.splice(del,1);
+        console.log("Task Deleted. ");
+    }
+    else if(req=="quit"){
+        console.log("Quitted Successfully ! :( ");
+        break;
+    }
+}
