6fa575e27514f95f24e8846811eebd613ebdfe24
manmay2 <imanmay2@gmail.com>
2024-10-17 20:44:05 +0530

    Assignment JS Part 5 done

---
+++ JS/JS_Part_5_Assignment.js
@@ -0,0 +1,27 @@
+// // Q1. Create a random function that will generate numbers representing a die.
+// console.log(Math.floor(Math.random()*6)+1);
+
+
+
+// // Q2. Create a car object and store the name,model,color as an object literal.
+// let car={
+//     name:"Manu",
+//     model:"Maruti",
+//     color:"Silver"
+// };
+// console.log(car.name);
+// console.log(car["name"]);
+
+
+
+//Q3. Create a object Person with name,age ,city.
+let Person={
+    name:"Manmay",
+    age:20,
+    city:"Frankfurt, Germany"
+};
+
+console.log(Person);
+Person.city="Germany";
+Person.Country="Luxembourg";
+console.log(Person);
