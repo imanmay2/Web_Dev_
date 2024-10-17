e5419a09f019dd8d173d21884b9323101266e2f7
manmay2 <imanmay2@gmail.com>
2024-10-17 17:29:22 +0530

    Nested Object Literals

--- JS/JS_part_5.js
+++ JS/JS_part_5.js
@@ -1,7 +1,22 @@
-let twitter={
-    username:"@imanmay2",
-    content:"#MyFirstProject",
-    likes:2000,
-    reposts:8,
-    tags:["@anwesha","@imanmay2"]
+// let twitter={
+//     username:"@imanmay2",
+//     content:"#MyFirstProject",
+//     likes:2000,
+//     reposts:8,
+//     tags:["@anwesha","@imanmay2"]
+// };
+
+
+
+let student={
+    manmay:{
+        name:"Manmay",
+        college:"VIT",
+        father_name:"Jatin Chakraborty"
+    },
+    anwesha:{
+        name:"Anwesha",
+        college:"VIT",
+        father_name:"Chiranjib Jana"
+    }
 };
