ad92811a5df23617c5f3bf25dece479a7363a0cc
manmay2 <imanmay2@gmail.com>
2024-10-17 20:00:51 +0530

    JS_part 5 updated

--- JS/JS_part_5.js
+++ JS/JS_part_5.js
@@ -1,25 +1,25 @@
-// let twitter={
-//     username:"@imanmay2",
-//     content:"#MyFirstProject",
-//     likes:2000,
-//     reposts:8,
-//     tags:["@anwesha","@imanmay2"]
-// };
+let twitter={
+    username:"@imanmay2",
+    content:"#MyFirstProject",
+    likes:2000,
+    reposts:8,
+    tags:["@anwesha","@imanmay2"]
+};
 
 
 
-// let student=[
-//     manmay={
-//         name:"Manmay",
-//         college:"VIT",
-//         father_name:"Jatin Chakraborty"
-//     },
-//     anwesha={
-//         name:"Anwesha",
-//         college:"VIT",
-//         father_name:"Chiranjib Jana"
-//     }
-// ];
+let student=[
+    manmay={
+        name:"Manmay",
+        college:"VIT",
+        father_name:"Jatin Chakraborty"
+    },
+    anwesha={
+        name:"Anwesha",
+        college:"VIT",
+        father_name:"Chiranjib Jana"
+    }
+];
 
 
 console.log(Math.floor(Math.random()*100)+1);
