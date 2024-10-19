410fd78bc95473bcd8baeadb0c6073c27e99e2d0
manmay2 <imanmay2@gmail.com>
2024-10-19 19:13:05 +0530

    JS_Part_6 Q2 done

---
+++ JS/JS_Part_6.js
@@ -0,0 +1,17 @@
+// // Q1. Create a function that will create a dice.
+// function dice(){
+//     console.log(`Dice number is : ${Math.floor(Math.random()*6)+1}`);
+// }
+
+// dice();
+
+
+// //Q2. Create a function that will print the average of 3 numbers as arguments.
+// function averageNum(num1,num2,num3){
+//     console.log("Average number is : ");
+//     console.log((num1+num2+num3)/3);
+// }
+
+// averageNum(1,2,3);
+
+
