7acb40d4be5943d0d3841e8fb1f53d0a243a2872
manmay2 <imanmay2@gmail.com>
2024-10-21 22:19:15 +0530

    Use of this keyword

---
+++ JS/JS_part_7.js
@@ -0,0 +1,12 @@
+//Use of this keyword.
+
+// const student={
+//     name:"Anwesha",
+//     eng:94,
+//     math:90,
+//     phy:90,
+//     getAvg(){
+//         console.log(`${this.name} has got an average marks of ${(this.eng+this.math+this.phy)/3}`);
+//     }
+// }.
+
