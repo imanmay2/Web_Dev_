58a2858f05b4898d24f94763280dbeeabfd519d9
manmay2 <imanmay2@gmail.com>
2024-10-22 21:19:04 +0530

    Q2 done

--- JS/JS_Part_7_Assignment.js
+++ JS/JS_Part_7_Assignment.js
@@ -1,6 +1,10 @@
-// Write a Arrow function that returns square of the number n.
+// Q1. Write a Arrow function that returns square of the number n.
 let square=(n)=>(
     n*n
 );
 
 
+// Q2. Write a function that prints hello world at an interval of 2s each.
+setInterval(()=>{
+    console.log("Hello World !");
+},2000);
