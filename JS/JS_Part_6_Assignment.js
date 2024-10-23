62d9f2e24f6a5ec50a07e30c6ec8d538a0e76eec
manmay2 <imanmay2@gmail.com>
2024-10-23 11:33:17 +0530

    let's start

--- JS/JS_Part_6_Assignment.js
+++ JS/JS_Part_6_Assignment.js
@@ -1,1 +1,9 @@
-// Q1. 
+// Q1. Write a Javascript function that returns elements larger than a number.
+let larger=function(arr,num){
+    for(let i of arr){
+        if(num<i){
+            console.log(i);
+        }
+        console.log();
+    }
+}
