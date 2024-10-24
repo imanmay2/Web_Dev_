3fc95113b7e91a49df60e5b59e72b282c64ac90a
manmay2 <imanmay2@gmail.com>
2024-10-25 00:38:51 +0530

    maximum number in the array.

--- JS/JS_Part_8.js
+++ JS/JS_Part_8.js
@@ -1,21 +1,21 @@
-// Trying with arr.forEach function.
-let arr=[1,2,3,4,5,6,7,8];
-print=function(el){
-    console.log(el);
-}
+// // Trying with arr.forEach function.
+// let arr=[1,2,3,4,5,6,7,8];
+// print=function(el){
+//     console.log(el);
+// }
 
 
 
-//creating an array with objects.
-let arr1=[
-    {
-        name:"Anwesha",
-        marks:94.4
-    },{
-        name:"Manmay",
-        marks:98
-    }
-];
+// //creating an array with objects.
+// let arr1=[
+//     {
+//         name:"Anwesha",
+//         marks:94.4
+//     },{
+//         name:"Manmay",
+//         marks:98
+//     }
+// ];
 
 
 // arr1.forEach(
@@ -56,3 +56,24 @@ let arr1=[
 // console.log(num.some((el)=>{
 //     return el%2==0;
 // }));
+
+
+
+// //Reduce function----
+// let arr=[1,2,3,4,5];
+// let reduced_arr=arr.reduce((res,ele)=>{
+//     return res+ele;
+// });
+// console.log(reduced_arr);
+
+
+
+// Find Maximum of the array using the Reduce Function of the array.
+let arr=[1,2,3,4,5,6,7,8];
+let maxi=arr.reduce((res,ele)=>{
+    if(res<ele){
+        return ele;
+    }
+});
+
+console.log(maxi);
