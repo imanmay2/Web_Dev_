362ed108e44656b9a45972599b2e70e64da1e3e2
manmay2 <imanmay2@gmail.com>
2024-10-30 11:56:43 +0530

    JS part 9 done

--- JS/JS_Part_9.js
+++ JS/JS_Part_9.js
@@ -1,1 +1,38 @@
-let 
+// let id_=document.getElementById("my_id");
+// console.log(id_.innerHTML);
+
+
+// //Manipulating Attributes.
+// let div_=document.querySelector("div");
+// console.log(div_.getAttribute("id"));
+// div_.setAttribute("class","my_Class");
+// console.log(div_.getAttribute("class"));
+
+
+
+// //Changing the style.
+// let div_=document.querySelector("div");
+// div_.style.backgroundColor="yellow";
+// div_.style.fontWeight=1000;
+
+
+
+// let ul_=document.querySelectorAll("ul li");   // returns a HTML Collection.
+// for(let i=0;i<ul_.length;i++){
+//     ul_[i].style.backgroundColor="pink";
+// }
+
+
+
+//Using ClassList.
+let div_=document.querySelector("div");
+div_.setAttribute("class","my_Class");
+console.log(div_.classList);
+div_.classList.add("class1");
+div_.classList.add("class2");
+console.log(div_.classList);
+div_.classList.remove("class1");
+console.log(div_.classList);
+console.log(div_.classList.contains("class2"));
+div_.classList.toggle("class2");
+console.log(div_.classList);
