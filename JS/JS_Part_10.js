4dbb61eeaf3f55f616c3245759fa2dc9c3ac916c
manmay2 <imanmay2@gmail.com>
2024-10-31 17:49:48 +0530

    yet to be completed

--- JS/JS_Part_10.js
+++ JS/JS_Part_10.js
@@ -6,10 +6,13 @@ function hello(){
     console.log("Hello");
 };
 for(let btn of btns){
-    btn.addEventListener("click",function(){
-        console.log("Button was clicked! ");
-    });
-    btn.addEventListener("dblclick",function(){
-        console.log("Button was double clicked! ");
+    // btn.addEventListener("click",function(){
+    //     console.log("Button was clicked! ");
+    // });
+    btn.addEventListener("mouseenter",function(){
+        console.log("Mouse Enter button ignited. ");
     });
 }
+
+
+//use of this function.
