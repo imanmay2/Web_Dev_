15ee144fbc3aadfe189eb984be7ae5c6cec7c357
manmay2 <imanmay2@gmail.com>
2024-10-31 12:34:43 +0530

    updated with Q2

---
+++ JS/JS_Part_9_Assignment.js
@@ -0,0 +1,13 @@
+// // Q1. Create a new input and button tag , in the button, Click Me! must be written.
+// let inp=document.createElement("input");
+// let btn=document.createElement("button");
+// let body=document.querySelector("body");
+// body.appendChild(inp);
+// body.appendChild(btn);
+// btn.innerText="Click Me!";
+
+//Change the placeholder value to "Username", and change the id of the button to "btn".
+let inp=document.createElement("input");
+let btn=document.createElement("button");
+inp.setAttribute("placeholder","username");
+btn.setAttribute("id","btn");
