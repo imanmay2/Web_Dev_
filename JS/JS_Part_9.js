7faa307b22ed1f56634e4f881e915ef149dd56f7
manmay2 <imanmay2@gmail.com>
2024-10-31 11:37:23 +0530

    Practise Q done

--- JS/JS_Part_9.js
+++ JS/JS_Part_9.js
@@ -38,3 +38,22 @@
 // console.log(div_.classList);
 
 
+let body=document.querySelector("body");
+let p_=document.createElement("p");
+body.appendChild(p_);
+p_.append("Hey I'm red!");
+p_.style.color="red";
+let h3=document.createElement("h3");
+body.appendChild(h3);
+h3.append("Hey I'm using blue h3");
+h3.style.color="blue";
+
+let div1=document.createElement("div");
+div1.style.backgroundColor="pink";
+body.appendChild(div1);
+let div2=document.createElement("div");
+div1.appendChild(div2);
+div2.append("I am a div");
+let p1=document.createElement("p");
+div1.appendChild("p1");
+p1.append("Me too!");
