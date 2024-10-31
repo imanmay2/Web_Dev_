07b5f95a77705fd4222af6373e2463f749f1fa61
manmay2 <imanmay2@gmail.com>
2024-10-31 11:42:25 +0530

    JS_Part 9 completed

--- JS/JS_Part_9.js
+++ JS/JS_Part_9.js
@@ -1,41 +1,41 @@
-// let id_=document.getElementById("my_id");
-// console.log(id_.innerHTML);
+let id_=document.getElementById("my_id");
+console.log(id_.innerHTML);
 
 
-// //Manipulating Attributes.
-// let div_=document.querySelector("div");
-// console.log(div_.getAttribute("id"));
-// div_.setAttribute("class","my_Class");
-// console.log(div_.getAttribute("class"));
-
-
-
-// //Changing the style.
-// let div_=document.querySelector("div");
-// div_.style.backgroundColor="yellow";
-// div_.style.fontWeight=1000;
+//Manipulating Attributes.
+let div_=document.querySelector("div");
+console.log(div_.getAttribute("id"));
+div_.setAttribute("class","my_Class");
+console.log(div_.getAttribute("class"));
 
 
 
-// let ul_=document.querySelectorAll("ul li");   // returns a HTML Collection.
-// for(let i=0;i<ul_.length;i++){
-//     ul_[i].style.backgroundColor="pink";
-// }
+//Changing the style.
+let div_=document.querySelector("div");
+div_.style.backgroundColor="yellow";
+div_.style.fontWeight=1000;
 
 
 
-// //Using ClassList.
-// let div_=document.querySelector("div");
-// div_.setAttribute("class","my_Class");
-// console.log(div_.classList);
-// div_.classList.add("class1");
-// div_.classList.add("class2");
-// console.log(div_.classList);
-// div_.classList.remove("class1");
-// console.log(div_.classList);
-// console.log(div_.classList.contains("class2"));
-// div_.classList.toggle("class2");
-// console.log(div_.classList);
+let ul_=document.querySelectorAll("ul li");   // returns a HTML Collection.
+for(let i=0;i<ul_.length;i++){
+    ul_[i].style.backgroundColor="pink";
+}
+
+
+
+//Using ClassList.
+let div_=document.querySelector("div");
+div_.setAttribute("class","my_Class");
+console.log(div_.classList);
+div_.classList.add("class1");
+div_.classList.add("class2");
+console.log(div_.classList);
+div_.classList.remove("class1");
+console.log(div_.classList);
+console.log(div_.classList.contains("class2"));
+div_.classList.toggle("class2");
+console.log(div_.classList);
 
 
 let body=document.querySelector("body");
@@ -51,9 +51,11 @@ h3.style.color="blue";
 let div1=document.createElement("div");
 div1.style.backgroundColor="pink";
 body.appendChild(div1);
-let div2=document.createElement("div");
+let div2=document.createElement("h2");
 div1.appendChild(div2);
 div2.append("I am a div");
+// let br=document.createElement("br");
+// div2.append(br);
 let p1=document.createElement("p");
-div1.appendChild("p1");
+div1.appendChild(p1);
 p1.append("Me too!");
