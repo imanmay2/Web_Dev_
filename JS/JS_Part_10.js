4462fa78f55929fb0f89210d09ac31fa4e3ea154
manmay2 <imanmay2@gmail.com>
2024-10-31 22:37:12 +0530

    updated

--- JS/JS_Part_10.js
+++ JS/JS_Part_10.js
@@ -1,18 +1,25 @@
-let btns=document.querySelectorAll("button");
-// btns.onclick=function(){
-//     console.log("Hello :)");
+// let btns=document.querySelectorAll("button");
+// // btns.onclick=function(){
+// //     console.log("Hello :)");
+// // };
+// function hello(){
+//     console.log("Hello");
 // };
-function hello(){
-    console.log("Hello");
-};
-for(let btn of btns){
-    // btn.addEventListener("click",function(){
-    //     console.log("Button was clicked! ");
-    // });
-    btn.addEventListener("mouseenter",function(){
-        console.log("Mouse Enter button ignited. ");
-    });
-}
+// for(let btn of btns){
+//     // btn.addEventListener("click",function(){
+//     //     console.log("Button was clicked! ");
+//     // });
+//     btn.addEventListener("mouseenter",function(){
+//         console.log("Mouse Enter button ignited. ");
+//     });
+// }
 
 
 //use of this function.
+btns=document.querySelectorAll("button");
+for(let btn of btns){
+    btn.addEventListener("click",function(){
+        btn.style.backgroundColor="blue";
+        console.log("hello");
+    });
+}
