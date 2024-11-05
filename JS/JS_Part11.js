e874e48ba560a99495088cf1f0ca51baf200e7a8
manmay2 <imanmay2@gmail.com>
2024-11-05 22:22:18 +0530

    callback hell yet t be destry

--- JS/JS_Part11.js
+++ JS/JS_Part11.js
@@ -1,14 +1,43 @@
-function one(){
-    return 1;
-}
+// function one(){
+//     return 1;
+// }
 
-function two(){
-    return one()+one();
-}
+// function two(){
+//     return one()+one();
+// }
 
-function three(){
-    let ans=two()+one();
-    console.log(ans);
-}
-three();
+// function three(){
+//     let ans=two()+one();
+//     console.log(ans);
+// }
+// three();
 
+
+
+//CallBack hell.
+let h1=document.querySelector("h1");
+// setTimeout(()=>{
+//     h1.style.color="red";
+// },1000);
+// setTimeout(()=>{
+//     h1.style.color="green";
+// },2000);
+// setTimeout(()=>{
+//     h1.style.color="blue";
+// },3000);
+
+function colorChange(color,delay,nextColorChange){
+    setTimeout(()=>{
+        h1.style.color=color;
+        if(nextColorChange){
+            ;
+        }
+    },delay);
+};
+
+
+colorChange("red",1000,()=>{
+    colorChange("green",1000,()=>{
+        colorChange("blue",1000);
+    });
+});
