3911fff7e7a8831137f68fbe9525f774e415431e
manmay2 <imanmay2@gmail.com>
2024-11-05 16:42:56 +0530

    bug yet to be fixed

--- JS/todo_miniproject.js
+++ JS/todo_miniproject.js
@@ -7,6 +7,16 @@ function addTask(){
     input_.setAttribute("placeholder","Enter Task");
     li=document.createElement("li");
     ul.appendChild(li);
-    li.append(task_);
+    // li.append(task_);
+    li.innerHTML=task_+"<Button>DELETE</Button>";
+    ul.addEventListener("click",function(event){
+        event.stopPropagation();
+        if(event.target.nodeName=="BUTTON"){
+            let listItem=this.parentElement;
+            listItem.remove();
+        }
+    });
+
+
 }
 btn.addEventListener("click",addTask);
