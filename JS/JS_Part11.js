6194918bfb9d29526e073d9f9e88f79514412c6f
manmay2 <imanmay2@gmail.com>
2024-11-06 14:10:03 +0530

    promises instead of callback hell

--- JS/JS_Part11.js
+++ JS/JS_Part11.js
@@ -1,70 +1,83 @@
-function one(){
-    return 1;
-}
+// function one(){
+//     return 1;
+// }
 
 
-function two(){
-    return one()+one();
-}
+// function two(){
+//     return one()+one();
+// }
 
-function three(){
-    let ans=two()+one();
-    console.log(ans);
-}
-three();
-
-
-
-// CallBack hell.
-let h1=document.querySelector("h1");
-setTimeout(()=>{
-    h1.style.color="red";
-},1000);
-setTimeout(()=>{
-    h1.style.color="green";
-},2000);
-setTimeout(()=>{
-    h1.style.color="blue";
-},3000);
+// function three(){
+//     let ans=two()+one();
+//     console.log(ans);
+// }
+// three();
 
 
 
-function colorChange(color,delay,nextColorChange){
-    setTimeout(()=>{
-        h1.style.color=color;
-        if(nextColorChange){
-            nextColorChange();
+// // CallBack hell.
+// let h1=document.querySelector("h1");
+// setTimeout(()=>{
+//     h1.style.color="red";
+// },1000);
+// setTimeout(()=>{
+//     h1.style.color="green";
+// },2000);
+// setTimeout(()=>{
+//     h1.style.color="blue";
+// },3000);
+
+
+
+// function colorChange(color,delay,nextColorChange){
+//     setTimeout(()=>{
+//         h1.style.color=color;
+//         if(nextColorChange){
+//             nextColorChange();
+//         }
+//     },delay);
+// };
+
+
+// colorChange("red",1000,()=>{
+//     colorChange("green",1000,()=>{
+//         colorChange("blue",1000);
+//     });
+// });
+
+
+// //Callback hell 2----Example.
+// function dB(data,success,failure){
+//     let internetSpeed=Math.floor(Math.random()*10)+1;
+//     if(internetSpeed>4){
+//         success();
+//     }
+//     else{
+//         failure();
+//     }
+// };
+
+
+// dB("hello",()=>{
+//     console.log("Success : Data Saved Successfully.");
+//     dB("hey",()=>{
+//         console.log("Success2 : Data Saved Suucessfully.");
+//     },()=>{
+//         console.log("Failure2 : Data failed Successfully");
+//     });
+// },()=>{
+//     console.log("Failure : Data failed to upload! ");
+// });
+
+
+function savetoDb(data){
+    return new Promise((resolve,reject)=>{
+        let internetSpeed=Math.floor(Math.random()*10)+1;
+        if(internetSpeed>4){
+            resolve("Success!");
         }
-    },delay);
-};
-
-
-colorChange("red",1000,()=>{
-    colorChange("green",1000,()=>{
-        colorChange("blue",1000);
+        else{
+            reject("Failed!");
+        }
     });
-});
-
-
-//Callback hell 2----Example.
-function dB(data,success,failure){
-    let internetSpeed=Math.floor(Math.random()*10)+1;
-    if(internetSpeed>4){
-        success();
-    }
-    else{
-        failure();
-    }
-};
-
-
-dB("hello",()=>{
-    console.log("Success : Data Saved Successfully.");
-    dB("hey",()=>{
-        console.log("Success2 : Data Saved Suucessfully.");
-    },()=>{
-        console.log("Failure2 : Data failed Successfully");
-    });
-},()=>{
-    console.log("Failure : Data failed to upload! ");
-});
+}
