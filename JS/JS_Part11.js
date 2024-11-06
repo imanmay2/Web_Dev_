e4a11637672ac0702b5642ab2efe41d556d781eb
manmay2 <imanmay2@gmail.com>
2024-11-06 11:50:33 +0530

    callback hell

--- JS/JS_Part11.js
+++ JS/JS_Part11.js
@@ -26,17 +26,40 @@ let h1=document.querySelector("h1");
 //     h1.style.color="blue";
 // },3000);
 
-function colorChange(color,delay,nextColorChange){
-    setTimeout(()=>{
-        h1.style.color=color;
-        if(nextColorChange){
-            nextColorChange();
-        }
-    },delay);
-};
+// function colorChange(color,delay,nextColorChange){
+//     setTimeout(()=>{
+//         h1.style.color=color;
+//         if(nextColorChange){
+//             nextColorChange();
+//         }
+//     },delay);
+// };
 
-colorChange("red",1000,()=>{
-    colorChange("green",1000,()=>{
-        colorChange("blue",1000);
+// colorChange("red",1000,()=>{
+//     colorChange("green",1000,()=>{
+//         colorChange("blue",1000);
+//     });
+// });
+
+
+//Callback hell 2----Example.
+function dB(data,success,failure){
+    let internetSpeed=Math.floor(Math.random()*10)+1;
+    if(internetSpeed>4){
+        success();
+    }
+    else{
+        failure();
+    }
+}
+
+dB("hello",()=>{
+    console.log("Success : Data Saved Successfully.");
+    dB("hey",()=>{
+        console.log("Success2 : Data Saved Suucessfully.");
+    },()=>{
+        console.log("Failure2 : Data failed Successfully");
     });
+},()=>{
+    console.log("Failure : Data failed to upload! ");
 });
