f6f6f176a4461f8f7f55f3d1de965f38a29a988d
manmay2 <imanmay2@gmail.com>
2024-11-06 11:35:06 +0530

    trying with callBack hell

--- JS/JS_Part11.js
+++ JS/JS_Part11.js
@@ -30,11 +30,10 @@ function colorChange(color,delay,nextColorChange){
     setTimeout(()=>{
         h1.style.color=color;
         if(nextColorChange){
-            ;
+            nextColorChange();
         }
     },delay);
 };
-
 
 colorChange("red",1000,()=>{
     colorChange("green",1000,()=>{
