12c0c173d20f8bf1706f402056b2e7a24cbe9b3e
manmay2 <imanmay2@gmail.com>
2024-11-07 19:57:14 +0530

    fetching data from API

--- JS/JS_Part_12.js
+++ JS/JS_Part_12.js
@@ -1,71 +1,99 @@
 
-//Converting normal function in asnyc one using "Async" keyword.
-// async function greet(){
-//     throw "404 Page not found!";
-//     return "hello";
-// }
+// //Converting normal function in asnyc one using "Async" keyword.
+// // async function greet(){
+// //     throw "404 Page not found!";
+// //     return "hello";
+// // }
 
-// greet()
-// .then((result)=>{
-//     console.log("Promise was resolved.");
-//     console.log("Result was : ",result);
-// })
-// .catch((err)=>{
-//     console.log("Week connection.",err);
-// });
+// // greet()
+// // .then((result)=>{
+// //     console.log("Promise was resolved.");
+// //     console.log("Result was : ",result);
+// // })
+// // .catch((err)=>{
+// //     console.log("Week connection.",err);
+// // });
 
-//Converting Arrow function into a async one.
-// let greet=async ()=>{
-//     throw "Week Connection";
-//     return "hello";
-// }
+// //Converting Arrow function into a async one.
+// // let greet=async ()=>{
+// //     throw "Week Connection";
+// //     return "hello";
+// // }
 
 
-// //Using Await keyword.
-// function getNum(){
+// // //Using Await keyword.
+// // function getNum(){
+// //     return new Promise((resolve,reject)=>{
+// //         setTimeout(()=>{
+// //             console.log(Math.floor(Math.random()*10)+1);
+// //             resolve("Resolved");
+// //         },1000);
+// //     });
+// // }
+
+
+
+// // async function demo(){
+// //     await getNum();
+// //     await getNum();
+// //     await getNum();
+// //     await getNum();
+// //     getNum();
+// // }
+
+
+
+// //Using Await function , in Color Changer.
+// let h1=document.querySelector("h1");
+// function colorChange(color,delay){
 //     return new Promise((resolve,reject)=>{
 //         setTimeout(()=>{
-//             console.log(Math.floor(Math.random()*10)+1);
-//             resolve("Resolved");
+//             let num=Math.floor(Math.random()*5)+1;
+//             if(num>3){
+//                 reject("Promise Rejected!!!");
+//             }
+//             h1.style.color=color;
+//             resolve("Color changed!!");
 //         },1000);
 //     });
 // }
 
 
-
-// async function demo(){
-//     await getNum();
-//     await getNum();
-//     await getNum();
-//     await getNum();
-//     getNum();
+// let changeColor=async ()=>{
+//     try{
+//         await colorChange("red",1000);
+//         await colorChange("green",1000);
+//         colorChange("blue",1000);
+//     }catch(err){
+//         console.log("Error found ",err);
+//     }
+//     console.log(5+5);
 // }
 
 
+//Fetching API using url.
+let url="https://catfact.ninja/fact";
+fetch(url)
+.then((res)=>{
+    return res.json();
+})
 
-//Using Await function , in Color Changer.
-let h1=document.querySelector("h1");
-function colorChange(color,delay){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            let num=Math.floor(Math.random()*5)+1;
-            if(num>3){
-                reject("Promise Rejected!!!");
-            }
-            h1.style.color=color;
-            resolve("Color changed!!");
-        },1000);
-    });
-}
+.then((data)=>{
+    console.log("data1 : ",data.fact);
+    console.log();
+    return fetch(url);
+})
 
+.then((res)=>{
+    return res.json();
+})
 
-let changeColor=async ()=>{
-    try{
-        await colorChange("red",1000);
-        await colorChange("green",1000);
-        colorChange("blue",1000);
-    }catch(err){
-        console.log("Error found ",err);
-    }
-    console.log(5+5);
-}
+.then((data)=>{
+    console.log("data2 : ",data.fact);
+})
+
+.catch((error)=>{
+    console.log("Error is : ",error);
+})
+
+console.log("Hello Happy ! ");
