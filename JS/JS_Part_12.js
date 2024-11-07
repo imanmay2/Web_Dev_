6d079f6d07005bcd6b99c3be4e3131e08ec4d172
manmay2 <imanmay2@gmail.com>
2024-11-07 07:27:04 +0530

    try and catch working successfully.

--- JS/JS_Part_12.js
+++ JS/JS_Part_12.js
@@ -48,6 +48,10 @@ let h1=document.querySelector("h1");
 function colorChange(color,delay){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
+            let num=Math.floor(Math.random()*5)+1;
+            if(num>3){
+                reject("Promise Rejected!!!");
+            }
             h1.style.color=color;
             resolve("Color changed!!");
         },1000);
@@ -55,7 +59,12 @@ function colorChange(color,delay){
 }
 
 let changeColor=async ()=>{
-    await colorChange("red",1000);
-    await colorChange("green",1000);
-    colorChange("blue",1000);
+    try{
+        await colorChange("red",1000);
+        await colorChange("green",1000);
+        colorChange("blue",1000);
+    }catch(err){
+        console.log("Error found ",err);
+    }
+    console.log(5+5);
 }
