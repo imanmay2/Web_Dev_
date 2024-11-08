d4771e1727581c18eb107e73d71de36e7fe86bb3
manmay2 <imanmay2@gmail.com>
2024-11-08 07:17:05 +0530

    Part 13 about to complete.

--- JS/JS_Part_13.js
+++ JS/JS_Part_13.js
@@ -1,24 +1,24 @@
-//Using axios instead of fetch the API.
-// let url="https://catfact.ninja/fact";
-// let btn=document.querySelector("button");
-// btn.addEventListener("click",async()=>{
-//     let fact=await getFact();
-//     console.log(fact);
-//     let body_=document.querySelector("body");
-//     let p=document.createElement("p");
-//     body_.append(p);
-//     p.innerText=fact;
-// });
-// async function getFact(){
-//     try{
-//         let res=await axios.get(url);
-//         return res.data.fact;
-//     }catch(err){
-//         // console.log("Error is - ",err);
-//         return "404-No Fact Found."
+// Using axios instead of fetch the API.
+let url="https://catfact.ninja/fact";
+let btn=document.querySelector("button");
+btn.addEventListener("click",async()=>{
+    let fact=await getFact();
+    console.log(fact);
+    let body_=document.querySelector("body");
+    let p=document.createElement("p");
+    body_.append(p);
+    p.innerText=fact;
+});
+async function getFact(){
+    try{
+        let res=await axios.get(url);
+        return res.data.fact;
+    }catch(err){
+        // console.log("Error is - ",err);
+        return "404-No Fact Found."
         
-//     }
-// }
+    }
+}
 
 
 //Using dog's API, fetch the random images of the dog.
@@ -40,4 +40,12 @@ async function getImage(){
     }catch(err){
         return "Image Not Found!";
     }
+}
+
+
+let url="https://dog.ceo/api/breeds/image/random";
+async function dog(){
+    let config={headers:{Accept:"application/json"}};
+    let res=await axios.get(url);
+    console.log(res);
 }
