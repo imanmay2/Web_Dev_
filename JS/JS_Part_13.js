eec5d168da24042bbbd1a5390f770d901c6e1ba5
manmay2 <imanmay2@gmail.com>
2024-11-08 07:00:16 +0530

    updated

--- JS/JS_Part_13.js
+++ JS/JS_Part_13.js
@@ -1,21 +1,43 @@
 //Using axios instead of fetch the API.
-let url="https://catfact.ninja/fact";
+// let url="https://catfact.ninja/fact";
+// let btn=document.querySelector("button");
+// btn.addEventListener("click",async()=>{
+//     let fact=await getFact();
+//     console.log(fact);
+//     let body_=document.querySelector("body");
+//     let p=document.createElement("p");
+//     body_.append(p);
+//     p.innerText=fact;
+// });
+// async function getFact(){
+//     try{
+//         let res=await axios.get(url);
+//         return res.data.fact;
+//     }catch(err){
+//         // console.log("Error is - ",err);
+//         return "404-No Fact Found."
+        
+//     }
+// }
+
+
+//Using dog's API, fetch the random images of the dog.
+let url="https://dog.ceo/api/breeds/image/random";
 let btn=document.querySelector("button");
+let img=document.querySelector("img");
 btn.addEventListener("click",async()=>{
-    let fact=await getFact();
-    console.log(fact);
-    let body_=document.querySelector("body");
-    let p=document.createElement("p");
-    body_.append(p);
-    p.innerText=fact;
+    try{
+        let imgSrc=await getImage();
+        img.setAttribute("src",imgSrc);
+    }catch{
+        img.setAttribute("alt",imgSrc);
+    }
 });
-async function getFact(){
+async function getImage(){
     try{
         let res=await axios.get(url);
-        return res.data.fact;
+        return res.data.message;
     }catch(err){
-        // console.log("Error is - ",err);
-        return "404-No Fact Found."
-        
+        return "Image Not Found!";
     }
 }
