6e47075ee97c647dd5d02552b68110286a0cb4ec
manmay2 <imanmay2@gmail.com>
2024-12-16 19:44:33 +0530

    middleWare learnt part_1

---
+++ MiddleWare/index.cjs
@@ -0,0 +1,49 @@
+const express=require("express");
+const app=express();
+const port=8080;
+
+
+//creating Middleware
+// app.use("/",(req,res,next)=>{
+//     console.log("Hi, this is the first middleware.");
+//     next();
+// });
+
+// app.listen(port,(req,res)=>{
+//     console.log("App is listening to : ",port);
+// });
+
+// //logger info
+// // app.use((req,res,next)=>{
+// //     req.responseTime=new Date(Date.now().toString());
+// //     console.log(req.method,req.path,req.responseTime,req.hostname);
+// // });
+
+// app.get("/",(req,res)=>{
+//     res.send("This is root");
+// });
+
+
+
+// app.get("/random",(req,res)=>{
+//     res.send("This is the random page");
+// });
+
+
+
+//Create a authentication middleware concept.
+app.listen(port,()=>{
+    console.log("App is listening to : ",port);
+});
+//creating the middle ware
+let checkAccess=(req,res,next)=>{
+    let {query}=req.query;
+    if(query==="giveaccess"){
+        next();
+    }
+    throw new Error("ACESS DENIED");
+};
+
+app.get("/api",checkAccess,(req,res)=>{
+    res.send("Here is your Data");
+});
