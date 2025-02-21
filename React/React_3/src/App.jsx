98a8cd08dc022daf5ece8b65481ef2552eb0e6d4
manmay2 <imanmay2@gmail.com>
2025-02-21 20:06:03 +0530

    double click working succesfully

---
+++ React/React_3/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+let handleClick = () => {
+  console.log("Button is clicked! ");
+}
+
+let handleHover = () => {
+  console.log("Hovered!");
+}
+
+let handledblClick=()=>{
+  console.log("Double Clicked!!");
+}
+export default function App() {
+  return (
+    <div>
+      <button onClick={handleClick}>Click Me!</button>
+      <p onMouseOver={handleHover}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa eius omnis tempora quam sit sequi architecto nemo numquam sed, quia quas velit saepe reiciendis. Illum maiores excepturi iure rerum voluptas.!</p>
+      <button onDoubleClick={handledblClick}>Double Click!</button>
+    </div>
+  );
+}
