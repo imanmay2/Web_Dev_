0ff8ae2b64800b61a77eb21fd55d9833dc64edd3
manmay2 <imanmay2@gmail.com>
2025-02-21 20:21:21 +0530

    event object submitted

--- React/React_3/src/App.jsx
+++ React/React_3/src/App.jsx
@@ -1,23 +1,15 @@
 import './App.css'
-let handleClick = () => {
-  console.log("Button is clicked! ");
+
+let submit=(event)=>{
+  event.preventDefault();
+  console.log("Form Submitted");
 }
 
-let handleHover = () => {
-  console.log("Hovered!");
-}
-
-let handledblClick=()=>{
-  console.log("Double Clicked!!");
-}
-export default function App() {
+export default function App(){
   return (
-    <div>
-      <button onClick={handleClick}>Click Me!</button>
-      <p onMouseOver={handleHover}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-        Ipsa eius omnis tempora quam sit sequi architecto nemo numquam sed, 
-        quia quas velit saepe reiciendis. Illum maiores excepturi iure rerum voluptas.!</p>
-      <button onDoubleClick={handledblClick}>Double Click!</button>
-    </div>
+    <form onSubmit={submit}>
+      <input type="text" />
+      <button>Submit</button>
+    </form>
   );
 }
