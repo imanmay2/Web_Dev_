d82992396060c4f95feea97c6398f119da939be9
manmay2 <imanmay2@gmail.com>
2025-02-22 14:32:46 +0530

    Todo to be done later

--- React/React_3/src/Todo.jsx
+++ React/React_3/src/Todo.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 function Todo(){
-    // let [arr,setArr]
-    return (<div>
+    let [arr,setArr]=useState([]);
+
+    let [task,setTask]=useState("");
+
+    let changeTodoValue=(event)=>{
+        console.log(event.target.value);
+    }
+
+    return (
+    <div>
         <h1>Add Todo's</h1>
-        <input type="text" /> <button>Submit</button>
+        <input type="text" onChange={changeTodoValue}/>&nbsp;&nbsp; <button>Submit</button>
     </div>
     );
-    
 }
 
 export {Todo};
