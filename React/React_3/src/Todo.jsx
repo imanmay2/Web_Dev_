531b0efcda534b36bc383d3dea1d919ec0e4575e
manmay2 <imanmay2@gmail.com>
2025-02-26 15:57:10 +0530

    Delete functionality working perfectly.

--- React/React_3/src/Todo.jsx
+++ React/React_3/src/Todo.jsx
@@ -1,25 +1,49 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
-function Todo(){
+function Todo() {
 
-    let [arr,setArr]=useState([{Task:"Sample-task",id:uuidv4()},{Task:"Programming",id:uuidv4()}]);
-    let [task,setTask]=useState("");
+    let [arr, setArr] = useState([{ Task: "Sample-task", id: uuidv4() }, { Task: "Programming", id: uuidv4() }]);
+    let [task, setTask] = useState("");
 
-    let trackChange=(event)=>{
-        setTask(()=>{
+    let trackChange = (event) => {
+        setTask(() => {
             return event.target.value;
         });
         console.log(task);
     }
 
-    let setChange=()=>{
-    //    setArr([...arr,{Task:task,id:uuidv4()}]);
-          setArr((arr)=>{
-            return arr.push({Task:task,id:uuidv4()});
-          });
-          console.log(arr);
+    let setChange = () => {
+        // setArr([...arr, { Task: task, id: uuidv4() }]);
+        setArr((prev) => {
+            return ([...prev, { Task: task, id: uuidv4() }]);
+        });
+        console.log(arr);
     }
+
+
+    let deleteTask=((id_)=>{
+
+        setArr((prev)=>{
+            return arr.filter((prev)=>prev.id!=id_);
+        });
+
+        console.log(arr);
+    //    console.log("This has to be deleted");
+    //    console.log(arr);
+    // let newArr=[];
+    // let ct=0;
+    // for(let i=0;i<arr.length;i++){
+    //     if(arr[i].id!=id_){
+    //         newArr[ct].Task=arr[i].Task;
+    //         newArr[ct].id=arr[i].id;
+    //     }
+    // }
+
+
+    // console.log(newArr);
+
+    })
 
 
     return (
@@ -28,12 +52,15 @@ function Todo(){
             <br />
             <input type="text" onChange={trackChange} /> <button onClick={setChange}>ADD</button>
             <ul>
-                {arr.map((arr)=>{
-                    return (<li key={arr.id}>{arr.Task}</li>);
+                {arr.map((list) => {
+                    return (
+                            <li key={list.id}>{list.Task}&nbsp;
+                            &nbsp; <button onClick={()=>deleteTask(list.id)}>Delete</button></li>
+                    );
                 })}
             </ul>
         </div>
     );
 }
 
-export {Todo};
+export { Todo };
