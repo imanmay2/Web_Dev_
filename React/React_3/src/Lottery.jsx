a29ff75b132616dd9c314b8482854b0199c5e70d
manmay2 <imanmay2@gmail.com>
2025-03-01 23:12:28 +0530

    yet to be fixed

--- React/React_3/src/Lottery.jsx
+++ React/React_3/src/Lottery.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 function Lottery() {
     let [s, setSum] = useState(0);
+    let [n, setNum] = useState(0);
     let getLottery = () => {
         let num = Math.floor(Math.random() * (999 - 100) + 100);
-        console.log(num);
+        setNum(num);
         let sum = 0;
         while (num != 0) {
             let r = num % 10;
@@ -16,7 +17,7 @@ function Lottery() {
     return (
         <div>
             {s === 15 ? <h2>Lottery ,Congratualtions You won!</h2> : <h2>Lottery</h2>}
-            Lottery Ticket = {s} <br />
+            Lottery Ticket = {n} <br />
             <br />
             <button onClick={getLottery}>Get New Ticket</button>
             
