e9789e9e1a0f843b5402c100b4f48bd78275401f
manmay2 <imanmay2@gmail.com>
2025-03-01 17:42:45 +0530

    Lottery Exercise done

--- React/React_3/src/App.jsx
+++ React/React_3/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import {Todo} from "./Todo.jsx";
+import { Lottery } from './Lottery.jsx';
 export default function App(){
 
   
   return (
     <div>
-      <Todo/>
+      <Lottery/>
     </div>
   );
 }
--- React/React_3/src/Lottery.jsx
+++ React/React_3/src/Lottery.jsx
@@ -1,29 +1,33 @@
 import { useState } from "react";
 
-let [s,setSum]=useState(0);
+function Lottery() {
+    let [s, setSum] = useState(0);
+    let getLottery = () => {
+        let num = Math.floor(Math.random() * (999 - 100) + 100);
+        console.log(num);
+        let sum = 0;
+        while (num != 0) {
+            let r = num % 10;
+            sum = sum + r;
+            num = Math.floor(num / 10);
+        }
 
-let getLottery=()=>{
-    let num=Math.floor(Math.random()*(999-100)+100);
-    console.log(num);
-    let sum=0;
-    while(num!=0){
-        let r=num%10;
-        sum=sum+r;
-        num=Math.floor(num/10);
+        setSum(sum);
     }
 
-    setSum(sum);
-}
-function Lottery(){
+
+
     return (
         <div>
+            {s === 15 ? <h2>Lottery ,Congratualtions You won!</h2> : <h2>Lottery</h2>}
+            Lottery Ticket = {s} <br />
+            <br />
             <button onClick={getLottery}>Get New Ticket</button>
-            {s===15 ? "<p>Lottery ,Congratualtions You won!</p>" : "<p>Lottery</p>"}
-            Lottery Ticket = {s}
+            
 
         </div>
     );
 }
 
 
-export {Lottery};
+export { Lottery };
