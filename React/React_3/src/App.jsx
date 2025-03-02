794bf0050130bef5304baf9f96445352f292c440
manmay2 <imanmay2@gmail.com>
2025-03-02 10:06:38 +0530

    Lottery one yet to be done

--- React/React_3/src/App.jsx
+++ React/React_3/src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
 import {Todo} from "./Todo.jsx";
 import { Lottery } from './Lottery.jsx';
-export default function App(){
-
-  
+export default function App(){  
   return (
     <div>
       <Lottery/>
