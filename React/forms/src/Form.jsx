316658cd44adf51090658bc32e301ee00e6606ce
manmay2 <imanmay2@gmail.com>
2025-05-03 17:29:59 +0530

    yet to be done

---
+++ React/forms/src/Form.jsx
@@ -0,0 +1,11 @@
+export default function Form(){
+
+
+    return (
+        <div>
+            <label htmlFor="name"></label>
+            <input type="text" placeholder="Enter your name: " id="name" /> <br />
+
+        </div>
+    )
+}
