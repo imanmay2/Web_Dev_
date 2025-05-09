2b46599cf6235bfa10d76617ce39d915ad47ca65
manmay2 <imanmay2@gmail.com>
2025-05-09 11:56:25 +0530

    Weather_API updated

---
+++ React/Weather_App/src/App.jsx
@@ -0,0 +1,16 @@
+import {Search} from "./Search.jsx";
+import './App.css';
+import { useState } from "react";
+
+
+function App() {
+// let [place,setPlace]=useState("");
+
+  return (
+    <div>
+      <Search/>
+    </div>
+  )
+}
+
+export default App;
