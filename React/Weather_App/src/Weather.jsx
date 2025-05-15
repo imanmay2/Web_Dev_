f962b6b449dc74788914158e313b7eac33e70286
manmay2 <imanmay2@gmail.com>
2025-05-15 23:01:18 +0530

    Weather_React_App

--- React/Weather_App/src/Weather.jsx
+++ React/Weather_App/src/Weather.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import { Title } from "./Title"
 import "./Weather.css"
 import { WeatherData } from "./WeatherData";
-
+import TextField from '@mui/material/TextField';
 
 function Weather() {
     const key = import.meta.env.VITE_API_KEY;
     const url = import.meta.env.VITE_API_URL;
+    let [err, setErr] = useState("false");
     let [result, setResult] = useState({
         name: "",
         country: "",
@@ -20,41 +21,57 @@ function Weather() {
     });
 
     let fetchCooridnates = async (city) => {
-        let response = await fetch(`${url}?q=${city}&appid=${key}`);
-        let jsonResponse = await response.json();
-        console.log(jsonResponse);
-        return jsonResponse;
+        try {
+            let response = await fetch(`${url}?q=${city}&appid=${key}`);
+            let jsonResponse = await response.json();
+            console.log(jsonResponse);
+            setErr("false");
+            return jsonResponse;
+        } catch (err) {
+            setErr("true");
+        }
     }
 
 
     let fetchData = async (lat, lon) => {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`);
-        let res = await response.json();
-        console.log(res);
-        setResult({
-            name: data.location,
-            country: res.sys.country,
-            temperature: res.main.temp,
-            humidity: res.main.humidity,
-            windSpeed: res.wind.speed,
-            pressure: res.main.pressure
-        });
+        try {
+            let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`);
+            let res = await response.json();
+            console.log(res);
+            setErr("false");
+            setResult({
+                name: data.location,
+                country: res.sys.country,
+                temperature: res.main.temp,
+                humidity: res.main.humidity,
+                windSpeed: res.wind.speed,
+                pressure: res.main.pressure
+            });
+        } catch {
+            setErr("true");
+        }
+
     }
 
 
     let handleSubmit = async (event) => {
-        event.preventDefault();
-        let dataSet = await fetchCooridnates(data.location);
-        console.log(dataSet[0].lat, dataSet[0].lon);
-        let lat = dataSet[0].lat;
-        let lon = dataSet[0].lon;
-        let dataSet2 = await fetchData(lat, lon);
-        console.log(dataSet2);
-        setData({
-            location: ""
-        });
+        try {
+            event.preventDefault();
+            let dataSet = await fetchCooridnates(data.location);
+            console.log(dataSet[0].lat, dataSet[0].lon);
+            let lat = dataSet[0].lat;
+            let lon = dataSet[0].lon;
+            let dataSet2 = await fetchData(lat, lon);
+            console.log(dataSet2);
+            setErr("false");
+            setData({
+                location: ""
+            });
+        } catch (err) {
+            setErr("true");
+        }
     }
-    let myLocation=()=>{
+    let myLocation = () => {
 
     }
 
@@ -68,16 +85,17 @@ function Weather() {
             <div><Title title="Weather App" /></div> <br />
 
             <div className='location'>
-                <input className="input_" type="text" placeholder="Enter City" name="location" value={data.location} onChange={handleChange} />
+                <TextField label="City" variant="outlined" className="input_" type="text" name="location" value={data.location} onChange={handleChange} />
+                {/* <input className="input_" type="text" placeholder="Enter City" name="location" value={data.location} onChange={handleChange} /> */}
                 <button onClick={handleSubmit} style={{ color: "black", backgroundColor: "#5be5f5", marginLeft: "2vh" }}>Search</button> <br />
                 <button onClick={myLocation} style={{ color: "black", backgroundColor: "#5be5f5", marginLeft: "2vh" }}>My Location</button>
 
 
             </div> <br />
-            
 
 
-             <WeatherData result={result} setResult={setResult}/>
+
+            <WeatherData result={result} err={err} />
         </div>
     )
 }
--- React/Weather_App/src/WeatherData.jsx
+++ React/Weather_App/src/WeatherData.jsx
@@ -1,9 +1,15 @@
 import "./Weather.css";
+import AcUnitRoundedIcon from '@mui/icons-material/AcUnitRounded';  //cold
+import LocalFireDepartmentRoundedIcon from '@mui/icons-material/LocalFireDepartmentRounded'; //heat
 
-function WeatherData({result,setResult}){
+
+
+function WeatherData({result,err}){
     return(
         <div>
-            {result.name != "" ? <h3> <br />{result.name},{result.country} <br />  <br /><img src="./image1.png" height="5vh" width="5vh" />{result.temperature}&#x2103;</h3> : null}
+
+            {(err=="true"? <p style={{color:"red"}}>No such City or Place found!</p>:null)}
+            {result.name != "" ? <h3> <br />{result.name},{result.country} {result.temperature>29? <LocalFireDepartmentRoundedIcon/> : <AcUnitRoundedIcon/>}<br />  <br />{result.temperature}&#x2103;</h3> : null}
 
 
             {result.name && <div className="temp">
@@ -12,7 +18,8 @@ function WeatherData({result,setResult}){
                 <div className="grey">Pressure <br /> <font className="text">{result.pressure} Pa</font></div>
             </div>}
 
-            {(result.name!="" && result.temperature>30) ? <p className="text">Temperature is high.</p>:<p className="text">Temperature is low.</p>}
+            {(result.temperature) ? 
+            (result.temperature>30)? <p className="text">Temperature is high.</p>:<p className="text">Temperature is low.</p>:null}
         </div>
     )
 }
