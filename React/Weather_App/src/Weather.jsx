a25669eb84a7ce2f703d82c454f8941033f42fbb
manmay2 <imanmay2@gmail.com>
2025-05-15 11:49:46 +0530

    yet to be deployed

--- React/Weather_App/src/Weather.jsx
+++ React/Weather_App/src/Weather.jsx
@@ -3,15 +3,57 @@ import {Title} from "./Title"
 import "./Weather.css"
 
 function Weather(){
+    const key=import.meta.env.VITE_API_KEY;
+    const url=import.meta.env.VITE_API_URL;
+    let [result,setResult]=useState({
+        name:"",
+        country:"",
+        temperature:"",
+        humidity:"",
+        windSpeed:"",
+        Pressure:""
+    });
     let [data,setData]=useState({
         location:""
     });
-    let handleSubmit=(event)=>{
+
+    let fetchCooridnates=async (city)=>{
+        let response=await fetch(`${url}?q=${city}&appid=${key}`);
+        let jsonResponse=await response.json();
+        console.log(jsonResponse);
+        return jsonResponse;
+    }
+
+
+    let fetchData=async (lat,lon)=>{
+        let response=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`);
+        let res=await response.json();
+        console.log(res);
+        // setResult({
+        //     name:data.location,
+        //     country:res.sys.country,
+        //     temperature:
+        // });
+
+    }
+
+    let handleSubmit=async (event)=>{
         event.preventDefault();
-        console.log(data.location);
-        setData({
+
+        
+            let dataSet=await fetchCooridnates(data.location);
+            console.log(dataSet[0].lat,dataSet[0].lon);
+            let lat=dataSet[0].lat;
+            let lon=dataSet[0].lon;
+            let dataSet2=await fetchData(lat,lon);
+            console.log(dataSet2);
+            setData({
             location:""
         });
+        
+
+        
+        
     }
 
     let handleChange=(event)=>{
@@ -26,7 +68,6 @@ function Weather(){
                 <input className="input_"  type="text" placeholder="Enter City" name="location" value={data.location} onChange={handleChange}/>
                 <button onClick={handleSubmit} style={{color:"black",backgroundColor:"#5be5f5",marginLeft:"2vh"}}>Search</button>
                 <div className="Data">
-
                 </div>
             </div>
         </div>
