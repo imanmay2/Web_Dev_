ddeb1335fd20edae354c6647e3e6064c6ddd14a3
manmay2 <imanmay2@gmail.com>
2025-05-16 23:08:28 +0530

    updated

--- React/Newsletter_UI/src/Newsletter.jsx
+++ React/Newsletter_UI/src/Newsletter.jsx
@@ -1,19 +1,24 @@
 import "./Newsletter.css";
-
-function NewsLetter(){
-    return(
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+function NewsLetter() {
+    return (
         <div className="main">
             <div className="header">
-               <div className="text"> News you can Trust.</div>
+                <div className="text"> News you can Trust.</div>
             </div>
             <div className="section">
-                <br /><br />
-                Stay up to date with the Latest! <br />
-                Subscribe to our newsletter for the latest news straight into your inbox. <br />
-
+                <br />
+                <font className="bold_">Stay up to date with the Latest!</font> <br />
+                Subscribe to our newsletter for the latest news straight into your inbox. <br /> <br />
+                <div className="email">
+                    
+                </div> <br /> <br />
+                <Button variant="contained" className="size_ color_btn">Contained</Button> <br /><br />
+                We value your privacy
             </div>
         </div>
     )
 }
 
-export {NewsLetter}
+export { NewsLetter }
