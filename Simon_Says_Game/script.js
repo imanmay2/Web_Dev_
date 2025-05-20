b49fb7b9f11fa40dc68256b93c6f5ae30504fc96
manmay2 <imanmay2@gmail.com>
2025-05-20 14:53:01 +0530

    Simon-Says-Game

--- Simon_Says_Game/script.js
+++ Simon_Says_Game/script.js
@@ -3,35 +3,33 @@ let level = 0;
 let game = [];
 let userGame = [];
 let count = 0;
+let end = false;
 let color = ["yellow", "red", "green", "purple"];
 
-function start(){
-    document.addEventListener("keypress", function start() {
-    if (started == false) {
-        console.log("Game started.");
-        started = true;
-        levelUp();
-    }
-});
-}
+
+
+//querySelectors--->>
+let body = document.querySelector("body");
+let p = document.querySelector("p");
+let h3 = document.querySelector("h3");
+let btns = document.querySelectorAll(".btn");
+let timer=document.querySelector(".timer");
 
 
 
-let end=false;
-
+//Functions.
 function checkGame(game, userGame) {
-    if(userGame.length!==game.length){
+    if (userGame.length !== game.length) {
         return false;
-    } else{
-        for(let i=0;i<game.length;i++){
-            if(game[0]!==userGame[0]){
+    } else {
+        for (let i = 0; i < game.length; i++) {
+            if (game[i] !== userGame[i]) {
                 return false;
             }
         }
     }
     return true;
 }
-
 
 
 
@@ -44,59 +42,85 @@ function btnflash(btn) {
 }
 
 
+function userflash(btn) {
+    console.log(btn);
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 150);
+}
+
+
 function btnPress() {
     let btn = this;   //returns the btn object.
-    // console.log(btn);
     userGame.push(btn.getAttribute("id"));
-    btnflash(btn);
-    if(userGame[count]!=game[count]){
-        end=true;
+    userflash(btn);
+
+    //check logic.
+    if (userGame[count] != game[count]) {
+        gameEnd();
+
+        setTimeout(resetGame,5000);
+
         return;
+    } else{
+        count++;
+        if(userGame.length===game.length && level===count){
+            count=0;
+            userGame=[];
+            levelUp();
+        }
     }
 }
 
 
-function gameEnd(){
-    let body=document.querySelector("body");
+function gameEnd() {
     body.classList.add("errorFlash");
-    setTimeout(function(){
+    setTimeout(function () {
         body.classList.remove("errorFlash");
-    },200);
+    }, 200);
+    p.innerText = "You score is : " + level;
+}
 
-
-    let p=document.querySelector("p");
-    p.innerText="You have scored : "+level-1;
+function resetGame() {
+    started = false;
+    level = 0;
+    game=[];
+    userGame=[];
+    count=0;
+    start();
 }
 
 
+function start() {
+    p.innerText = "";
+    h3.innerText = "Press Any Key to Start the Game.";
+    document.addEventListener("keypress", function start() {
+        if (started == false) {
+            console.log("Game started.");
+            started = true;
+            for (let btn of btns) {
+            btn.addEventListener("click", btnPress);
+    }
+            levelUp();
+        }
+    });
+}
+
+
+
+
 function levelUp() {
-    let h3 = document.querySelector("h3");
     level++;
+    console.log(level);
     h3.innerText = `Level ${level}`;
-    let randIdx = Math.floor(Math.random() * 4);
+    let randIdx = Math.floor(Math.random() * 4);    //generates random number between 0-3.
     let randBtn = color[randIdx];
     console.log(randIdx, randBtn);
     game.push(randBtn);
     let btn = document.querySelector(`.${randBtn}`);
     btnflash(btn);
-
-
-
-    let btns = document.querySelectorAll(".btn");
-    for (let btn of btns) {
-        btn.addEventListener("click", btnPress);
-    }
-
-    if(checkGame(game,userGame) && level===count && end===false){
-        count=0;
-        userGame=[];
-        levelUp();
-    } else if(end===true){
-        gameEnd();
-        started=false;
-        count=0;
-        level=0;
-    }
+    
 }
 
 
