3c6ba0c1d886b6f1e9b8299f356ec028ea2fb1aa
manmay2 <imanmay2@gmail.com>
2025-05-20 11:15:43 +0530

    Yet to be done

--- Simon_Says_Game/script.js
+++ Simon_Says_Game/script.js
@@ -1,28 +1,103 @@
-let started=false;
-let count=0;
+let started = false;
+let level = 0;
+let game = [];
+let userGame = [];
+let count = 0;
+let color = ["yellow", "red", "green", "purple"];
 
-
-document.addEventListener("keypress",function start(){
-    if(started==false){
+function start(){
+    document.addEventListener("keypress", function start() {
+    if (started == false) {
         console.log("Game started.");
-        started=true;
+        started = true;
         levelUp();
     }
 });
+}
 
 
-function btnflash(btn){
+
+let end=false;
+
+function checkGame(game, userGame) {
+    if(userGame.length!==game.length){
+        return false;
+    } else{
+        for(let i=0;i<game.length;i++){
+            if(game[0]!==userGame[0]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+
+
+function btnflash(btn) {
+    console.log(btn);
     btn.classList.add("flash");
-    setTimeout(function remove(){
+    setTimeout(function () {
         btn.classList.remove("flash");
-    },250);
+    }, 150);
 }
 
-function levelUp(){
-    alert("User must start the game.");
-    let h3=document.querySelector("h3");
-    count++;
-    h3.innerText=`Level ${count}`;
-    let btns=document.querySelectorAll("btn");
-    
+
+function btnPress() {
+    let btn = this;   //returns the btn object.
+    // console.log(btn);
+    userGame.push(btn.getAttribute("id"));
+    btnflash(btn);
+    if(userGame[count]!=game[count]){
+        end=true;
+        return;
+    }
 }
+
+
+function gameEnd(){
+    let body=document.querySelector("body");
+    body.classList.add("errorFlash");
+    setTimeout(function(){
+        body.classList.remove("errorFlash");
+    },200);
+
+
+    let p=document.querySelector("p");
+    p.innerText="You have scored : "+level-1;
+}
+
+
+function levelUp() {
+    let h3 = document.querySelector("h3");
+    level++;
+    h3.innerText = `Level ${level}`;
+    let randIdx = Math.floor(Math.random() * 4);
+    let randBtn = color[randIdx];
+    console.log(randIdx, randBtn);
+    game.push(randBtn);
+    let btn = document.querySelector(`.${randBtn}`);
+    btnflash(btn);
+
+
+
+    let btns = document.querySelectorAll(".btn");
+    for (let btn of btns) {
+        btn.addEventListener("click", btnPress);
+    }
+
+    if(checkGame(game,userGame) && level===count && end===false){
+        count=0;
+        userGame=[];
+        levelUp();
+    } else if(end===true){
+        gameEnd();
+        started=false;
+        count=0;
+        level=0;
+    }
+}
+
+
+start();
