de52a8803bb7c1db8f93989881e6834516e547f9
manmay2 <imanmay2@gmail.com>
2025-05-24 18:50:57 +0530

    useRef and React Router deployed successfully.

--- Exp_2/experiment/src/components/Navbar.jsx
+++ Exp_2/experiment/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ function Navbar(){
     return(
         <div>
             <nav>
-                
+                <div className="navbar">
                     <Link to="/"><li>Home</li></Link>
                     <Link to="/Login"><li>Login</li></Link>
                     <Link><li>Profile</li></Link>
-             
+                </div>
             </nav>
         </div>
     )
