c99275c5048a7f3d1029dc35e47bcd55d60b807b
manmay2 <imanmay2@gmail.com>
2025-05-29 10:44:55 +0530

    typeScript started

--- Typescript/intro.ts
+++ Typescript/intro.ts
@@ -1,11 +1,1 @@
-let myNum:number=1234;
-myNum.toFixed(2);
-console.log(typeof(myNum));
-console.log(typeof(myNum.toFixed(2)));
-console.log(parseFloat(myNum.toFixed(2)));
-
-console.log((myNum));
-console.log((myNum.toFixed(2)));
-console.log(parseFloat(myNum.toFixed(2)));
-
-export {};
+let myNum=123;
