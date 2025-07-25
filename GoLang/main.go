fa014d569d2ccb05b0c04c7e9fe2634b516e4bd2
manmay2 <imanmay2@gmail.com>
2025-07-25 21:55:16 +0530

    Array Slicing done

--- GoLang/main.go
+++ GoLang/main.go
@@ -1,8 +1,34 @@
 package main
+
 import "fmt"
 
 func main(){
-	var input string
-	fmt.Scanln(&input);
-	fmt.Printf("Hello %v",input)
+	// var username string
+	
+	// fmt.Scanln(&username)
+
+	// fmt.Println("Welcome to GoLang ",username)
+
+	//Array operations
+	var usernames=[6]string{}  //creating an empty array.
+	usernames[0]="imanmay2"
+	usernames[1]="ianwesha2"
+	usernames[2]="imanshree2"
+
+
+	fmt.Println("Usernames are: ")
+	fmt.Println(usernames[0])
+	fmt.Println(usernames[1])
+	fmt.Println(usernames[2])
+
+	//Slice Operations
+
+	fmt.Println("Slice Operations")
+	var names []string
+	names=append(names,"Manmay Chakaborty")
+	names=append(names,"Anwesha Chakraborty")
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	
 }
