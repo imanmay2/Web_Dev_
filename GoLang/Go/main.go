28a45648d45c058356f913b087f500d443c222d9
manmay2 <imanmay2@gmail.com>
2025-08-25 11:03:54 +0530

    System Restarted.

---
+++ GoLang/Go/main.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Hello ");
+	fmt.Println("Hey, I am Manmay Chakraborty.");
+	fmt.Println("A passionate Backend Developer.");
+}
--- GoLang/go.mod
+++
@@ -1,2 +0,0 @@
-module GoLang
-go 1.24.5
