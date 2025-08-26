174f576cdaf78cd47521365a88e2b1310a759227
manmay2 <imanmay2@gmail.com>
2025-08-26 23:10:33 +0530

    remaining with the struct in Go

--- GoLang/Go1/main.go
+++ GoLang/Go1/main.go
@@ -8,4 +8,35 @@ import (
 func main(){
 	fmt.Println("Welcome to Helper Package!!!!");
 	helper.GreetUser();
+
+
+	// MAP 
+	students:=make(map[string]int);
+
+	students["Manmay"]=90;
+	students["Anwesha"]=80;
+	students["Jagriti"]=70;
+
+	fmt.Println("1st Loop : ");
+	for key,val:=range students{
+		fmt.Printf("Key : %v Value is : %v",key,val);
+	}
+	fmt.Println();
+	students["Anwesha"]=91;
+	fmt.Println("2nd Loop : ");
+	for key,val:=range students{
+		fmt.Printf("Key : %v Value is : %v",key,val);
+		fmt.Println();
+	}
+	fmt.Println();
+
+	delete(students,"Jagriti");
+
+	fmt.Println("3rd Loop : ");
+	for key,val:=range students{
+		fmt.Printf("Key : %v Value is : %v",key,val);
+		fmt.Println();
+	}
+	fmt.Println();
+
 }
