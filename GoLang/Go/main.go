98e8132d5724b6a39a45a8fb8428c4d762d68146
manmay2 <imanmay2@gmail.com>
2025-08-26 09:30:29 +0530

    for loop , if-else done in Go

--- GoLang/Go/main.go
+++ GoLang/Go/main.go
@@ -3,11 +3,78 @@ import "fmt"
 
 func main(){
 	
-	var username string;
-	var age int;
-	fmt.Printf("Enter the username : ");
-	fmt.Scan(&username);
-	fmt.Printf("Enter the age : ");
-	fmt.Scan(&age);
-	fmt.Printf("Hello %v , Your age is %v",username,age);
+	// var username string;
+	// var age int;
+	// fmt.Printf("Enter the username : ");
+	// fmt.Scan(&username);
+	// fmt.Printf("Enter the age : ");
+	// fmt.Scan(&age);
+	// fmt.Printf("Hello %v , Your age is %v",username,age);
+
+
+	//Arrays
+	fmt.Println("Welcome to GoLang.");
+	var arr [5]int;
+	arr[0]=1;
+	fmt.Println(arr);
+	arr[1]=2;
+	fmt.Println(arr);
+	arr[2]=3;
+	fmt.Println(arr);
+
+	fmt.Println("Length of the array : ",len(arr));
+	fmt.Println("Capacity of the array : ",cap(arr));
+
+
+	//Slices
+	fmt.Println("-----Slices ----- ");
+	var bookings []string;
+	bookings=append(bookings,"Manmay","Loves","Anwesha");
+
+	fmt.Println(bookings[1]);
+	fmt.Println(bookings);
+
+
+	//Q1. to store natural numbers in the array.
+	arr1:=[10]int{};
+	for i:=0;i<len(arr1);i++ {
+		arr1[i]=i+1;
+	}
+	fmt.Println(arr1);
+
+
+	//Q2. Creates the slice from the array , that contains only even numbers.
+	slice:=[]int{};
+
+	for idx,val:=range arr1{
+		if(arr1[idx]%2==0){
+			slice=append(slice,val);
+		}
+	}
+	fmt.Println("Slice Operation : ");
+	fmt.Println(slice);
+
+
+	//Q3. use for loop to print both the array as well as slice.
+	fmt.Println("Array Elements : - >");
+	for i:=0;i<len(arr1);i++{
+		fmt.Printf("%v ",arr1[i]);
+	} 
+	fmt.Println();
+
+	fmt.Println("Slice Elements : - >");
+
+	for i:=0;i<len(slice);i++{
+		fmt.Printf("%v ",slice[i]);
+	}
+	fmt.Println();
+
+
+	//Sum of the elements in the slice.
+	sum:=0;
+	for i:=0;i<len(slice);i++{
+		sum=sum+slice[i];
+	}
+
+	fmt.Println("Sum of the Slice Elements is: ",sum);
 }
