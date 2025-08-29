22a97893cedad6fdae2259cfa7e57fb459f6ac89
manmay2 <imanmay2@gmail.com>
2025-08-29 22:39:51 +0530

    POST REQUEST working finally.

---
+++ GoLang/Go_Gin/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"fmt"
+)
+
+
+type User struct{
+	Name string `json:"name" binding:"required"`
+	Email string `json:"email" binding:"required,email"`
+	Age int `json:"age"`
+}
+
+func main(){
+	app:=gin.Default();
+
+	//get request
+	app.GET("/getData",func(ctx *gin.Context){
+		ctx.JSON(200,gin.H{"message":"Welcome to GoLang"});
+	})
+
+
+	app.GET("/user/:id",func(ctx *gin.Context){
+		id:=ctx.Param("id");
+		ctx.String(200,"User ID : %s",id);
+	})
+
+	
+	app.POST("/setData",func(ctx *gin.Context){
+		var userData User;
+
+		//binding the incoming request.
+		if err:=ctx.ShouldBindJSON(&userData);
+		err!=nil{
+			ctx.JSON(400,gin.H{"Error":err.Error()});
+			return;
+		}
+
+		fmt.Println(userData);
+
+		ctx.JSON(200,gin.H{"Message":"Data saved successfully."});
+	})
+
+	app.Run(":8080");
+}
