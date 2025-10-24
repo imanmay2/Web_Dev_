d81b3010d15b972691664c34efc84c8c6cf8c654
manmay2 <imanmay2@gmail.com>
2025-10-24 15:33:00 +0530

    Nextjs done for now.

--- Nextjs/layouts/my-app/src/app/components/navbar.jsx
+++ Nextjs/layouts/my-app/src/app/components/navbar.jsx
@@ -1,5 +1,11 @@
+"use client"
+
+import { usePathname } from "next/navigation";
+
 export default function Navbar(){
+    let path=usePathname();
     return(
-        <div>Welcome to the Navbar. </div>
+        <div>Welcome to the Navbar.  You are in : {path}</div>
+        
     )
 }
