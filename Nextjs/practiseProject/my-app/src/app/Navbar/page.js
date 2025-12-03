95ee218e0b166bf84dd88fdeb32f01b6ea6be1c0
manmay2 <imanmay2@gmail.com>
2025-12-03 23:14:09 +0530

    useState in next.js

--- Nextjs/practiseProject/my-app/src/app/Navbar/page.js
+++ Nextjs/practiseProject/my-app/src/app/Navbar/page.js
@@ -1,12 +1,22 @@
-import Link from "next/link";
+"use client"
 
+import Link from "next/link";
+import { useState } from "react";
 export default function Navbar(){
+    let [count,setCount]=useState(0);
+    let increase=()=>{
+
+        setCount(count+1);
+        console.log(count);
+    }
     return(
         <>
-        <h1>Welcome to our app. </h1>
-
+        <h1>Welcome to our app.</h1>
         <Link href="/About">About</Link>
         <Link href="/profile">Profile</Link>
+
+        <p>Count is : {count}</p>
+        <button onClick={increase}>Click</button>
         </>
     )
 }
--- Nextjs/practiseProject/my-app/src/app/page.js
+++ Nextjs/practiseProject/my-app/src/app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
 import Navbar from "./Navbar/page";
 
 export default function Home() {
   return (
    <>
-   <Navbar></Navbar>
+   <Navbar/>
    </>
   );
 }
