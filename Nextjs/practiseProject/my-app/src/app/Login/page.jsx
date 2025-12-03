d6e60e8f65a7312ce12bfbecb77916edb6e3a624
manmay2 <imanmay2@gmail.com>
2025-12-04 00:23:27 +0530

    form data updation done

---
+++ Nextjs/practiseProject/my-app/src/app/Login/page.jsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useState } from "react";
+export default function Login(){
+    let [form,setForm]=useState({
+        username:"",
+        email:"",
+        password:""
+    });
+
+    let handleChange=(e)=>{
+        setForm({...form,[e.target.name]:e.target.value})
+    }
+
+    let submit=(e)=>{
+        e.preventDefault();
+        console.log("Form Data is : ");
+        console.log("Name is : "+form.username);
+        console.log("Email is : "+form.email);
+        console.log("Password is : "+form.password);
+        setForm({username:"",email:"",password:""});
+    }
+
+    return(
+        <div>
+            <center>
+                <div>
+                    <label htmlFor="user">Username</label>
+                    <input type="text" id="user" name="username" value={form.username} onChange={handleChange}/> <br /> <br />
+
+                    <label htmlFor="email">Email : </label>
+                    <input type="text" id="email" name="email"  value={form.email} onChange={handleChange}/> <br /> <br />
+
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password"  value={form.password} onChange={handleChange}/> <br /> <br />
+
+                    <button onClick={submit}>Submit</button>
+                </div>
+            </center>
+        </div>
+    )
+}
--- Nextjs/practiseProject/my-app/src/app/Navbar/page.jsx
+++ Nextjs/practiseProject/my-app/src/app/Navbar/page.jsx
@@ -5,16 +5,15 @@ import { useState } from "react";
 export default function Navbar(){
     let [count,setCount]=useState(0);
     let increase=()=>{
-
         setCount(count+1);
         console.log(count);
     }
     return(
         <>
         <h1>Welcome to our app.</h1>
-        <Link href="/About">About</Link>
-        <Link href="/profile">Profile</Link>
-
+        <Link href="/About">About</Link> <br />
+        <Link href="/profile">Profile</Link> <br />
+        <Link href="/Login">Login</Link> <br />
         <p>Count is : {count}</p>
         <button onClick={increase}>Click</button>
         </>
