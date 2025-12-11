21970760c2706ba4a1029e27b03d1be17b2de2e0
manmay2 <imanmay2@gmail.com>
2025-12-11 23:02:36 +0530

    update to be fixed..

--- TypeScript/practise.js
+++ TypeScript/practise.js
@@ -40,3 +40,55 @@ var printObj = function (userDetails) {
 printObj({ n: "Manmay", age: 24 });
 var newObj = funcObj({ n: "Manmay", age: 24 });
 printObj(newObj);
+console.log("restarted");
+console.log("restarted...");
+console.log();
+var myUser = {
+    _id: "S123",
+    name: "Manmay",
+    age: 24
+};
+console.log(myUser._id);
+console.log(myUser.name);
+console.log(myUser.age);
+console.log(myUser.licence);
+//Arrays
+var arr1 = [];
+for (var i = 1; i <= 8; i++) {
+    arr1.push(i);
+}
+var displayArr = function (arr) {
+    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+        var i = arr_1[_i];
+        console.log(i);
+    }
+};
+console.log("Elements in the array are follows : ");
+displayArr(arr1);
+//array of objects .
+var arrObj = [];
+arrObj.push({ n: "Anwesha", age: 21 });
+console.log(arrObj[0].n);
+console.log(arrObj[0].age);
+// 2D array in typescript.
+var arr2DFunc = function (arr) {
+    for (var i = 1; i <= 3; i++) {
+        arr[i] = [];
+        for (var j = 1; j <= 3; j++) {
+            arr[i].push(j);
+        }
+    }
+};
+var funcPrint2D = function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            console.log(arr[i][j] + " ");
+        }
+        console.log();
+    }
+};
+var arr12 = [];
+console.log("2D array is as follows : ");
+arr2DFunc(arr12);
+console.log("wec");
+funcPrint2D(arr12);
--- TypeScript/practise.ts
+++ TypeScript/practise.ts
@@ -57,4 +57,81 @@ printObj({n:"Manmay",age:24});
 let newObj:user=funcObj({n:"Manmay",age:24});
 printObj(newObj);
 
+console.log("restarted");
+
+console.log("restarted...");
+
+console.log();
+
+type student={
+    readonly _id:string,
+    name:string,
+    age:number,
+    licence?:boolean
+}
+
+let myUser:student={
+    _id:"S123",
+    name:"Manmay",
+    age:24
+}
+
+console.log(myUser._id);
+console.log(myUser.name);
+console.log(myUser.age);
+console.log(myUser.licence);
+
+
+//Arrays
+
+let arr1:Array<number>=[];
+for(let i=1;i<=8;i++){
+    arr1.push(i);
+}
+
+
+let displayArr=(arr:Array<number>):void=>{
+    for(let i of arr){
+        console.log(i);
+    }
+}
+
+console.log("Elements in the array are follows : ");
+displayArr(arr1);
+
+
+//array of objects .
+let arrObj:Array<user>=[];
+
+arrObj.push({n:"Anwesha",age:21});
+
+console.log(arrObj[0].n);
+console.log(arrObj[0].age);
+
+
+// 2D array in typescript.
+let arr2DFunc=(arr:Array<Array<number>>):void=>{
+    for(let i=1;i<=3;i++){
+        arr[i]=[];
+        for(let j=1;j<=3;j++){
+            arr[i].push(j);
+        }
+    }
+}
+
+let funcPrint2D=(arr:Array<Array<number>>):void=>{
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr[i].length;j++){
+            console.log(arr[i][j]+" ");
+        }
+        console.log();
+    }
+}
+let arr12:Array<Array<number>>=[];
+
+console.log("2D array is as follows : ");
+arr2DFunc(arr12);
+console.log("wec");
+funcPrint2D(arr12);
+
 export {};
