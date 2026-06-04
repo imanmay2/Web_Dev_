eca412b4dc84ea331374d77bee00ad411721debe
Manmay Chakraborty <imanmay2@gmail.com>
2026-06-04 13:34:10 +0530

    Sockets--> different Room testing done

--- Sockets/server/app.js
+++ Sockets/server/app.js
@@ -18,9 +18,25 @@ app.use(express.urlencoded());
 
 io.on("connection",(socket)=>{
     console.log(socket.id);
-    socket.on("sendMsg",(data)=>{
-        console.log(data.name);
-        socket.emit("receiveMsg",{msg:"Data recieved"});
+    // socket.on("sendMsg",(data)=>{
+    //     console.log(data.name);
+    //     // socket.emit("receiveMsg",{msg:"Data recieved"});
+    // });
+
+    socket.on("join_room",(room_id)=>{
+        socket.join(room_id);
+        console.log(room_id+" is connected with "+socket.id);
+    });
+  
+    socket.on("sendMsgRoom",(data)=>{
+        console.log("Room_id : "+data.room_id);
+        console.log("Msg received from client : "+data.msg);
+
+        io.to(data.room_id).emit("receive_","Message received successfully.");
+    });
+    
+    socket.on("disconnect",()=>{
+        console.log(socket.id+" is disconnected");
     });
 });
 
